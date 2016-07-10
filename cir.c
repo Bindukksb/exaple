b23e64632b0e103a70339a12978af42d5e51d912
Bindukksb <bindukksb@gmail.com>
2016-07-10 11:13:14 +0530

    Create cir.c

---
+++ cir.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,i,j;
+printf("Enter the calculation:");
+scanf("%d",&a);
+printf("Enter the n value");
+scanf("%d",&b);
+printf("Enter the pair:");
+scanf("%d",&c);
+for(i=0;i<2*c;i++);
+{
+for(j=1;j=2*b-a;j++)
+if(c!=j)
+{
+printf("Enter the pair",c);
+printf("Enter the for day",j++,c);
+}
+}
+printf("Enter the calculation",a);
+return 0;
+}
